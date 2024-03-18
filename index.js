15ac11a06cf71c6f4ae90d2937ed1ab092a51663
thealexandru2292@gmail.com <thealexandru2292@gmail.com>
2024-03-18 17:33:06 +0200

    Parsing our Planet Data

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const fs = require('fs');
 const results = [];
 
 fs.createReadStream('kepler_data.csv')
+//connect parse() with fs together by pipe()
+//it transform data from stream (0101) to bytes data (text)
+    .pipe(parse(
+        {
+            //dependign on who wrote the csv file put the comment sign and we need to indicate which character is the commented line
+            comment: '#',
+            columns: true,//set columns as key=>value pairs
+        }
+    ))
     .on('data', (data) => {
         results.push(data);
     })
