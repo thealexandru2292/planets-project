c06126f2f5e2649a10438a83f3580823f3bb4297
thealexandru2292@gmail.com <thealexandru2292@gmail.com>
2024-03-18 17:46:32 +0200

    finding/filtering habitable planets

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+
+function isHabitablePlanet(planet)
+{
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
 
 fs.createReadStream('kepler_data.csv')
 //connect parse() with fs together by pipe()
@@ -14,12 +22,18 @@ fs.createReadStream('kepler_data.csv')
         }
     ))
     .on('data', (data) => {
-        results.push(data);
+
+        if(isHabitablePlanet(data))
+        {
+            habitablePlanets.push(data);
+        }
+        
     })
     .on('error', (err) => {
         console.log(err);
     })
     .on('end', () => {
-        console.log(results);
+        console.log(habitablePlanets);
+        console.log(`${habitablePlanets.length} habitable planets found`);
         console.log('done');
     })
